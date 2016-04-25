fe2625a6dab1f16d242cb70224822648bc2d7e1f
John Roberts <john@cavucode.com>
2016-04-25 11:03:53 -0400

    aggregates

--- projects/avdata_test/avdata_test.R
+++ projects/avdata_test/avdata_test.R
@@ -20,25 +20,36 @@ aircraft[1:100, c("dprt.state", "regis.no")]
 # !is.na()
 # != ""
 
+# TODO filter on data frame aircraft[is.null(),] vs dplyr filter()
+
 ac_by_country <- 
   aircraft[, c("dprt.country", "dprt.state", "regis.no")] %>% 
   filter(dprt.country != "", !is.null(dprt.country), !is.na(dprt.country)) %>%
   group_by(dprt.country) %>% 
   summarise(count=n()) %>% 
-  arrange(desc(count),dprt.country)
+  arrange(desc(count),dprt.country) 
 
+
+# shop top 5 countries
+top_n(ac_by_country, 5)
 
 #TODO show percent of total by country
 
 
+
 # count of rows grouped by dprt.state for USA only
-ac_by_usastate <- aircraft[aircraft$dprt.country=="USA", c("dprt.country", "dprt.state", "regis.no")] %>% 
+ac_by_usastate <- 
+  aircraft[aircraft$dprt.country=="USA", c("dprt.country", "dprt.state", "regis.no")] %>% 
   group_by(dprt.country, dprt.state) %>% 
   summarise(count=n()) %>% 
   arrange(desc(count),dprt.state)
 
 
-
+#TODO get sum, mean, max, dev of state counts
+min(ac_by_usastate$count, na.rm = FALSE)
+max(ac_by_usastate$count, na.rm = FALSE)
+mean(ac_by_usastate$count, na.rm = FALSE)
+sd(ac_by_usastate$count, na.rm = FALSE)
 
 
 
