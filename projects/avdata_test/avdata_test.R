f87649d7796333ca6950936a6111070aa498f1f8
John Roberts <john@cavucode.com>
2016-04-25 09:53:34 -0400

    test script for 4/25 mentor call

--- projects/avdata_test/avdata_test.R
+++ projects/avdata_test/avdata_test.R
@@ -1,7 +1,29 @@
-install.packages("dplyr")
+#install.packages("dplyr")
 library(dplyr)
-aircraft <- mdb.get("")
+library(Hmisc)
+setwd("~/Downloads");
+
+# load aircraft table from avall.mdb file
+aircraft <- mdb.get("avall.mdb", tables="aircraft")
+
+#view first 100 rows
+aircraft[1:100, c("dprt.state", "regis.no")]
 
 
 
-aircraft[1:20, c("dprt.state", "regis.no")] %>% group_by(dprt.state) %>% summarize(count=n()) %>% arrange(desc(count), dprt.state)
+# count of rows grouped by dprt.state for USA only
+loc_filter_field <- "dprt.country"
+loc_field <- "dprt.state"
+aircraft[aircraft$dprt.country=="USA", c("dprt.country", "dprt.state", "regis.no")] %>% 
+  group_by(dprt.state) %>% 
+  summarise(count=n()) %>% 
+  arrange(desc(count), dprt.state)
+
+
+# TODO - how to use variables as field names, and concat table and variable field
+loc_filter_field <- "dprt.country"
+loc_field <- "dprt.state"
+aircraft[aircraft$dprt.country=="USA", c("dprt.country", "dprt.state", "regis.no")] %>% 
+  group_by(dprt.state) %>% 
+  summarise(count=n()) %>% 
+  arrange(desc(count), dprt.state)
